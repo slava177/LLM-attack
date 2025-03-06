5f2a347de58f5b9c2f8d42b560d415889a10de8e
Ziyi <ziyizhan@login-gpu.cm.cluster>
2025-03-06 15:23:58 -0500

    vim update

--- LLM-finetune/slurm_run.sh
+++ LLM-finetune/slurm_run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -p GPU-shared
 #SBATCH -t 5:00:00
-#SBATCH --gpus=H100:1
-#SBATCH --output = "./output.txt"
+#SBATCH --gres gpu:H100:1
+#SBATCH --output="output.txt"
 
-#source ../../LLM_attack_env/bin/activate
+# source ../../LLM_attack_env/bin/activate
 python Lora.py
